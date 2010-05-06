5bb439f0377425da4e0ed8df5162024233ed785b
Carlos <cartucho@moria-dev.(none)>
2010-05-06 07:14:49 +0100

    Updated license headers

--- bateria_pruebas.rb
+++ bateria_pruebas.rb
@@ -1,6 +1,6 @@
 # This file is part of vcodec_bench.
 # 
-# EseaTetris is free software: you can redistribute it and/or modify
+# vcodec_bench is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
@@ -11,7 +11,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with EseaTetris. If not, see <http://www.gnu.org/licenses/>.
+# along with vcodec_bench. If not, see <http://www.gnu.org/licenses/>.
 
 class Prueba
   attr_accessor :input
